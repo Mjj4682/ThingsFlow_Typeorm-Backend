c6afedd759e908870ace87ea12a3a1dbe2482dfa
JungJin Moon <jjmoon4682@gmail.com>
2022-11-04 00:46:22 +0900

    feat: 게시글 작성 기능(비밀번호)

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -7,6 +7,19 @@ const createPosts = asyncHandler(async (req, res) => {
   if (!title || !content || !password || !userId) {
     throw new errorConstructor(400, "필수값이 없습니다.");
   }
+  if (title.length > 20 || content.length > 200) {
+    throw new errorConstructor(
+      400,
+      "제목은 최대 20자, 본문은 최대 200자 입니다."
+    );
+  }
+  const reg = /^(?=.*?[0-9]).{6,}$/;
+  if (!reg.test(password)) {
+    throw new errorConstructor(
+      400,
+      "비밀번호는 6자 이상, 숫자가 1개 포함되어야 합니다."
+    );
+  }
   await postsService.createPosts(title, content, password, userId);
   res.status(201).json({ message: "created post" });
 });
--- src/services/postsService.ts
+++ src/services/postsService.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import database from "../dataSource";
 import { Posts } from "../entities/Posts";
 import { User } from "../entities/User";
@@ -8,12 +9,16 @@ const createPosts = async (
   password: string,
   userId: number
 ) => {
+  const saltRound = 8;
+  const salt = await bcrypt.genSalt(saltRound);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = new User();
   user.id = userId;
   const posts = new Posts();
   posts.title = title;
   posts.content = content;
-  posts.password = password;
+  posts.password = hashedPassword;
   posts.user = user;
   await database.manager.save(posts);
 };
